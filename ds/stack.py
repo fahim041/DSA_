3bea828c327a8e7feff2e24543f199b8c10c679d
fahim041 <fahimalsafi@gmail.com>
2024-06-13 19:29:46 -0600

    stack test case

--- ds/stack.py
+++ ds/stack.py
@@ -5,8 +5,26 @@ class Stack:
     def push(self, val):
         self.stack.append(val)
 
-    def pop(self, val):
+    def pop(self):
         self.stack.pop()
 
     def peek(self):
         return self.stack[-1]
+    
+# test cases
+def test_stack():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+
+    stack.pop()
+    stack.pop()
+
+    assert stack.peek() == 3
+
+    print('All test cases pass')
+
+test_stack()
